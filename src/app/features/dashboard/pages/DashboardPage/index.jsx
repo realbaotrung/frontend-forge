afcaea7f1d88c186fb1a7eafae17c320e571d589
realbaotrung <realbaotrung@gmail.com>
2022-04-18 10:02:38 +0700

    Update check by role, move folder Uploadfile

--- src/app/features/dashboard/pages/DashboardPage/index.jsx
+++ src/app/features/dashboard/pages/DashboardPage/index.jsx
@@ -1,16 +1,22 @@
 import {Box, Heading, Text, VStack} from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
 import NavBar from '../../../../components/Navbar';
-import UploadFiles from './features/UploadFiles';
+import { selectRole, selectUser } from '../../../../slices/auth/selectors';
+import AdminPage from './pages/AdminPage/AdminPage';
+import UserPage from './pages/UserPage/UserPage';
 
 // TODO: Bug, findDOMNode at InternalSubMenuList Component (by Antd)
 
+
 export default function DashboardPage() {
+
+  const role = useSelector(selectRole);
+
+  const showPageDependByRole = (role === 'admin' ? <AdminPage /> : <UserPage />);
   return (
-  <Box as='section'>
+  <>
     <NavBar />
-    <UploadFiles />
-    <Heading as='h1'>dashboard Page</Heading>
-    <Text>this is a dashboard page.</Text>
-  </Box>
+    {showPageDependByRole}
+  </>
   );
 }
